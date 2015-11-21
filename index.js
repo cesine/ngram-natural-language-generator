0ddd749e37438eb3e066596536888b51d2a912a5
cesine <cesine@yahoo.com>
2015-11-20 23:57:15 -0500

    generating setences

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/local/bin/node
 
-
 var ngrams = require('./lib/ngrams').buildNgrams;
 var nlg = require('./lib/nlg').generateText;
 
@@ -10,9 +9,11 @@ var main = function() {
     ngrams({
       filename: process.argv[2]
     }, function(model){
-      console.log('Generated model ', model);
+      // console.log('Generated model ', model);
 
-      var sentence = nlg();
+      var sentence = nlg({
+        model: model
+      });
       console.log(sentence);
     });
   }
