03d14d906e606b48624cb1c5c5d9140f19e3e82c
cesine <cesine@yahoo.com>
2015-12-07 23:26:37 -0500

    formatting

--- test/index-spec.js
+++ test/index-spec.js
@@ -4,30 +4,30 @@ var ngramNaturalLanguageGenerator = require('../').generator;
 
 describe('ngram natural language generator', function() {
 
-	it('should load', function() {
-		expect(ngramNaturalLanguageGenerator).toBeDefined();
-	});
+  it('should load', function() {
+    expect(ngramNaturalLanguageGenerator).toBeDefined();
+  });
 
-	describe('options', function() {
+  describe('options', function() {
 
-		it('should support sentence length', function(done) {
-			var options = {
-				filename: 'samples/jabberwocky.txt',
-				model: {
-					maxLength: 3,
-					minLength: 1
-				}
-			};
-			ngramNaturalLanguageGenerator(options, function(err, sentence) {
+    it('should support sentence length', function(done) {
+      var options = {
+        filename: 'samples/jabberwocky.txt',
+        model: {
+          maxLength: 3,
+          minLength: 1
+        }
+      };
+      ngramNaturalLanguageGenerator(options, function(err, sentence) {
 
-				expect(err).toBeNull();
-				expect(sentence).toBeDefined();
-				console.log('result ', sentence);
-				expect(sentence).toContain(' ');
-				expect(sentence.split(' ').length >= 1).toBeTruthy();
+        expect(err).toBeNull();
+        expect(sentence).toBeDefined();
+        console.log('result ', sentence);
+        expect(sentence).toContain(' ');
+        expect(sentence.split(' ').length >= 1).toBeTruthy();
 
-				done();
-			});
-		});
-	});
+        done();
+      });
+    });
+  });
 });
--- test/nlg-spec.js
+++ test/nlg-spec.js
@@ -3,41 +3,41 @@
 var nlg = require('../lib/nlg').nlg;
 
 var TINY_MODEL = {
-	'.': ['Colorless'],
-	'Colorless': ['green'],
-	'green': ['ideas'],
-	'ideas': ['sleep'],
-	'sleep': ['furiously'],
-	'furiously': ['.']
+  '.': ['Colorless'],
+  'Colorless': ['green'],
+  'green': ['ideas'],
+  'ideas': ['sleep'],
+  'sleep': ['furiously'],
+  'furiously': ['.']
 };
 
 describe('nlg', function() {
 
-	it('should load', function() {
-		expect(nlg).toBeDefined();
-	});
+  it('should load', function() {
+    expect(nlg).toBeDefined();
+  });
 
-	describe('options', function() {
+  describe('options', function() {
 
-		it('should support sentence length', function() {
-			var sentence = nlg({
-				data: TINY_MODEL,
-				maxLength: 12,
-				minLength: 5
-			});
+    it('should support sentence length', function() {
+      var sentence = nlg({
+        data: TINY_MODEL,
+        maxLength: 12,
+        minLength: 5
+      });
 
-			expect(sentence).toEqual('Colorless green ideas sleep furiously. Colorless green ideas sleep furiously.');
-		});
+      expect(sentence).toEqual('Colorless green ideas sleep furiously. Colorless green ideas sleep furiously.');
+    });
 
-		it('should support sentence length', function() {
-			var sentence = nlg({
-				data: TINY_MODEL,
-				maxLength: 3,
-				minLength: 1
-			});
+    it('should support sentence length', function() {
+      var sentence = nlg({
+        data: TINY_MODEL,
+        maxLength: 3,
+        minLength: 1
+      });
 
-			expect(sentence).toEqual('Colorless green ideas sleep furiously.');
-		});
+      expect(sentence).toEqual('Colorless green ideas sleep furiously.');
+    });
 
-	});
+  });
 });
